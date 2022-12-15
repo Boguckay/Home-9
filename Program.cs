52b36a5146cd78ce4b002f974169637d2c1c404d
Boguckaya_is <Boguckay@mail.ru>
2022-12-15 16:27:58 +0300

    Задача 68

--- Program.cs
+++ Program.cs
@@ -25,29 +25,55 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-Console.Write("Введите число M: "); 
- int m = Convert.ToInt32(Console.ReadLine()); 
+// Console.Write("Введите число M: "); 
+//  int m = Convert.ToInt32(Console.ReadLine()); 
  
- Console.Write("Введите число N: "); 
- int n = Convert.ToInt32(Console.ReadLine()); 
+//  Console.Write("Введите число N: "); 
+//  int n = Convert.ToInt32(Console.ReadLine()); 
  
- SumFromMToN(m, n); 
+//  SumFromMToN(m, n); 
  
- void SumFromMToN(int m, int n) 
- { 
-     Console.Write(SumMN(m - 1, n)); 
- } 
-     int SumMN(int m, int n) 
- { 
-     int res = m; 
-     if (m == n) 
-         return 0; 
-     else 
-     { 
-         m++; 
-         res = m + SumMN(m, n); 
-         return res; 
-     } 
- } 
+//  void SumFromMToN(int m, int n) 
+//  { 
+//      Console.Write(SumMN(m - 1, n)); 
+//  } 
+//      int SumMN(int m, int n) 
+//  { 
+//      int res = m; 
+//      if (m == n) 
+//          return 0; 
+//      else 
+//      { 
+//          m++; 
+//          res = m + SumMN(m, n); 
+//          return res; 
+//      } 
+//  } 
 
 
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+int m = InputNumbers("Введите m: "); 
+int n = InputNumbers("Введите n: "); 
+ 
+ int functionAkkerman = Ack(m, n); 
+
+ Console.Write($"Функция Аккермана = {functionAkkerman} "); 
+ 
+ int Ack(int m, int n) 
+ { 
+   if (m == 0) return n + 1; 
+   else if (n == 0) return Ack(m - 1, 1); 
+   else return Ack(m - 1, Ack(m, n - 1)); 
+ } 
+ 
+ int InputNumbers(string input)  
+ { 
+   Console.Write(input); 
+   int output = Convert.ToInt32(Console.ReadLine()); 
+   return output; 
+ } 
